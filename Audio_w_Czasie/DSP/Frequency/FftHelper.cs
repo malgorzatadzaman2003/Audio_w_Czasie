1f2e3a4885931fb766f72ca567f6f580ae346005
Małgorzata Dżaman <01180662@pw.edu.pl>
2026-04-23 16:02:24 +0200

    add FFT for whole signal and selected frame

--- Audio_w_Czasie/DSP/Frequency/FftHelper.cs
+++ Audio_w_Czasie/DSP/Frequency/FftHelper.cs
@@ -1,12 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Audio_w_Czasie.DSP.Frequency
 {
-    internal class FftHelper
+    public static class FftHelper
     {
+        public static int NextPow2(int n)
+        {
+            int p = 1;
+            while (p < n) p <<= 1;
+            return p;
+        }
+
+        public static Complex[] ComputeFft(double[] signal)
+        {
+            int n = NextPow2(signal.Length);
+            Complex[] buf = new Complex[n];
+
+            for (int i = 0; i < signal.Length; i++)
+                buf[i] = new Complex(signal[i], 0);
+
+            FFTInPlace(buf);
+            return buf;
+        }
+
+        public static void FFTInPlace(Complex[] buffer)
+        {
+            int n = buffer.Length;
+            int bits = (int)Math.Log2(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                int j = ReverseBits(i, bits);
+                if (j > i)
+                    (buffer[i], buffer[j]) = (buffer[j], buffer[i]);
+            }
+
+            for (int len = 2; len <= n; len <<= 1)
+            {
+                double ang = -2 * Math.PI / len;
+                Complex wLen = new Complex(Math.Cos(ang), Math.Sin(ang));
+
+                for (int i = 0; i < n; i += len)
+                {
+                    Complex w = Complex.One;
+                    for (int j = 0; j < len / 2; j++)
+                    {
+                        Complex u = buffer[i + j];
+                        Complex v = w * buffer[i + j + len / 2];
+                        buffer[i + j] = u + v;
+                        buffer[i + j + len / 2] = u - v;
+                        w *= wLen;
+                    }
+                }
+            }
+        }
+
+        private static int ReverseBits(int x, int bits)
+        {
+            int y = 0;
+            for (int i = 0; i < bits; i++)
+            {
+                y = (y << 1) | (x & 1);
+                x >>= 1;
+            }
+            return y;
+        }
+
+        public static void GetMagnitudeSpectrum(
+            Complex[] fft,
+            int sampleRate,
+            out double[] freqs,
+            out double[] mags)
+        {
+            int half = fft.Length / 2;
+            freqs = new double[half];
+            mags = new double[half];
+
+            for (int k = 0; k < half; k++)
+            {
+                freqs[k] = (double)k * sampleRate / fft.Length;
+                mags[k] = fft[k].Magnitude;
+            }
+        }
     }
 }
