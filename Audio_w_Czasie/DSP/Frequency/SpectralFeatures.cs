377df4717e81aab6e7705eb4fbb53e7a7dc659af
Małgorzata Dżaman <01180662@pw.edu.pl>
2026-04-23 16:10:57 +0200

    add spectral frame features

--- Audio_w_Czasie/DSP/Frequency/SpectralFeatures.cs
+++ Audio_w_Czasie/DSP/Frequency/SpectralFeatures.cs
@@ -6,7 +6,78 @@ using System.Threading.Tasks;
 
 namespace Audio_w_Czasie.DSP.Frequency
 {
-    internal class SpectralFeatures
+    public sealed class SpectralFrameFeatures
     {
+        public double SpectralVolume { get; init; }
+        public double CentroidHz { get; init; }
+        public double BandwidthHz { get; init; }
+        public double[] BandEnergy { get; init; } = Array.Empty<double>();
+        public double[] BandEnergyRatio { get; init; } = Array.Empty<double>();
+        public double SpectralFlatness { get; init; }
+        public double SpectralCrestFactor { get; init; }
     }
+
+    public static class SpectralFeatures
+    {
+        public static SpectralFrameFeatures Compute(double[] freqs, double[] mags)
+        {
+            double eps = 1e-12;
+            double[] psd = mags.Select(v => v * v).ToArray();
+
+            double spectralVolume = psd.Sum() / psd.Length;
+
+            double psdSum = psd.Sum() + eps;
+            double centroid = 0;
+            for (int i = 0; i < freqs.Length; i++)
+                centroid += freqs[i] * psd[i];
+            centroid /= psdSum;
+
+            double bw = 0;
+            for (int i = 0; i < freqs.Length; i++)
+            {
+                double d = freqs[i] - centroid;
+                bw += d * d * psd[i];
+            }
+            bw = Math.Sqrt(bw / psdSum);
+
+            double[] bandLimits = { 0, 630, 1720, 4400, 11025 };
+            double[] be = new double[4];
+            for (int b = 0; b < 4; b++)
+            {
+                for (int i = 0; i < freqs.Length; i++)
+                {
+                    if (freqs[i] >= bandLimits[b] && freqs[i] < bandLimits[b + 1])
+                        be[b] += psd[i];
+                }
+            }
+
+            double totalBe = be.Sum() + eps;
+            double[] ber = be.Select(x => x / totalBe).ToArray();
+
+            double geo = 1.0;
+            int count = 0;
+            double arith = psd.Average();
+            foreach (var p in psd)
+            {
+                geo += Math.Log(p + eps);
+                count++;
+            }
+            geo = Math.Exp(geo / count);
+            double sfm = arith < eps ? 1.0 : geo / arith;
+
+            double scf = arith < eps ? 0.0 : psd.Max() / arith;
+
+            return new SpectralFrameFeatures
+            {
+                SpectralVolume = spectralVolume,
+                CentroidHz = centroid,
+                BandwidthHz = bw,
+                BandEnergy = be,
+                BandEnergyRatio = ber,
+                SpectralFlatness = sfm,
+                SpectralCrestFactor = scf
+            };
+        }
+    }
+
 }
